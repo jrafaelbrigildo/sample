34047867a2b938fb1ced9478757eac96c5091a8c
jrafaelbrigildo <rafaelbrigildo@gmail.com>
2024-02-05 19:35:12 +0800

    after changes

---
+++ scripts/simple_calculator.js
@@ -0,0 +1,26 @@
+const calcDisplay = document.getElementById("calcDisplay");
+
+function appendToDisplay(input){
+    calcDisplay.value += input; 
+}
+
+function clearDisplay(){
+    calcDisplay.value = '';
+}
+
+function calculate(){
+    try {
+            // Replace percentage symbol with its decimal representation
+        let expression = calcDisplay.value.replace(/%/g, '/100');
+
+            // Replace exponentiation (^) with the power expression
+            expression = expression.replace(/\^/g, '**');
+
+            // Replace square root (√) with the Math.sqrt function including parentheses
+            expression = expression.replace(/√(\d+)/g, 'Math.sqrt($1)');
+
+        calcDisplay.value = eval(expression);
+    } catch (error) {
+        calcDisplay.value = "Error";
+    }
+}
